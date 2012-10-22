92d39501bcca3f71df58a2fd1216ab47a151fbc9
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2012-10-22 16:35:10 +0200

    First time working. Lots of debug messages though

--- code/mr.erl
+++ code/mr.erl
@@ -13,16 +13,19 @@ start(N) ->
     {Reducer, Mappers} = init(N),
     {ok, spawn(fun() -> coordinator_loop(Reducer, Mappers) end)}.
 
+stop(Pid) -> stop_async(Pid).
 
-stop(Pid) -> ....
-
-job(CPid, MapFun, RedFun, RedInit, Data) -> ....
-
+job(CPid, MapFun, RedFun, RedInit, Data) -> 
+  job_sync(CPid, MapFun, RedFun, RedInit, Data).
 
 %%%% Internal implementation
 
+init(N) -> Reducer = spawn(fun reducer_loop/0),
+  {Reducer, init_mappers(N, Reducer)}.
 
-init(N) -> ....
+init_mappers(0, _) -> [];
+init_mappers(N, Reducer) -> 
+  [spawn(fun() -> mapper_loop(Reducer) end) | init_mappers(N-1,Reducer)].
 
 
 %% synchronous communication
@@ -43,6 +46,14 @@ reply_ok(From) ->
 reply_ok(From, Msg) ->
     reply(From, {ok, Msg}).
 
+setup(Pid, Fun) ->
+  rpc(Pid ,{setup, Fun}).
+
+job_sync(CPid, MapFun, RedFun, RedInit, Data) ->
+  rpc(CPid, {job, {MapFun, RedFun, RedInit ,Data}}).
+
+%setup_sync(Pid, Fun) ->
+%  rpc(Pid, {setup, Fun}).
 
 %% asynchronous communication
 
@@ -58,19 +69,45 @@ data_async(Pid, D) ->
 setup_async(Pid, Fun) ->
     info(Pid, {setup, Fun}).
 
-
 %%% Coordinator
 
 coordinator_loop(Reducer, Mappers) ->
-    receive
-	{From, stop} ->
+  receive
+	  {From, stop} ->
 	    io:format("~p stopping~n", [self()]),
 	    lists:foreach(fun stop_async/1, Mappers),
 	    stop_async(Reducer),
 	    reply_ok(From);
-	....
-    end.
+    {From, {job, JobData}} ->
+      {MapFun, RedFun, RedInit, Data} = JobData,
+      %%setup the reducer and mappers
+      %% Set the reducer into receive mode
+      %% Is there a reason to do it synced?
+      setup(Reducer, {RedFun, RedInit, length(Data)}),
+      io:format("reducer initialised"),
+      lists:foreach(fun(M) -> setup_async(M,MapFun) end,Mappers),
+      io:format("mappers initialised"),
+      %% Then perform the mappings
+      send_data(Mappers,Data),
+      coordinator_loop(Reducer, Mappers, From)
+  end.
 
+coordinator_loop(Reducer, Mappers, Sender) ->
+    receive
+  {From, stop} ->
+      io:format("~p stopping~n", [self()]),
+	    lists:foreach(fun stop_async/1, Mappers),
+	    stop_async(Reducer),
+	    reply_ok(From);
+  {data, D} ->
+      %% Return ok and a result to the creator
+      reply_ok(Sender,D),
+      %% Return back to idle state
+      coordinator_loop(Reducer, Mappers);
+  {_, {ok, Msg}} ->
+      reply_ok(Sender, Msg),
+      coordinator_loop(Reducer, Mappers)
+    end.
 
 send_data(Mappers, Data) ->
     send_loop(Mappers, Mappers, Data).
@@ -90,24 +127,67 @@ reducer_loop() ->
 	stop -> 
 	    io:format("Reducer ~p stopping~n", [self()]),
 	    ok;
-        ....
+  {From, {setup, {Fun, Acc, Missing}}} ->
+      %%Fun - the anonymous reducer function
+      %%Acc - The accumulator
+      %%Missing - The number of transmissions missing
+      io:format("Setup received"),
+      reply_ok(From),
+      gather_data_from_mappers(From, Fun, Acc, Missing);
+  Unknown ->
+      io:format("unknown message in reducer: ~p~n",[Unknown]),
+      reducer_loop()
     end.
 
-gather_data_from_mappers(Fun, Acc, Missing) ->
+gather_data_from_mappers(Coordinator, _, Acc, 0) ->
+  io:format("I am now finished"),
+  reply_ok(Coordinator, Acc),
+  reducer_loop();
+gather_data_from_mappers(Coordinator, Fun, Acc, Missing) ->
+  io:format("Now in correct stage"),
     receive
-	...
+  stop ->
+      io:format("Reducer ~p stopping~n", [self()]),
+      ok;
+  {data, D} -> %Should probably be something else
+      %% Reduce the data here
+      io:format("data received, processing"),
+      NewAcc = Fun(D, Acc),
+      gather_data_from_mappers(Coordinator, Fun, NewAcc, Missing - 1);
+  Unknown ->
+      io:format("unknown message in gather_data_from_mappers: ~p~n",[Unknown]),
+      gather_data_from_mappers(Coordinator, Fun, Acc, Missing)
     end.
 
 
 %%% Mapper
 
+mapper_loop(Reducer) ->
+    receive
+  stop ->
+      io:format("Mapper ~p stopping~n", [self()]),
+      ok;
+  {setup, Fun} ->
+      io:format("Mapper setup ok"),
+      mapper_loop(Reducer, Fun);
+  Unknown ->
+	    io:format("unknown message in mapper: ~p~n",[Unknown]), 
+	    mapper_loop(Reducer)
+    end.
+
 mapper_loop(Reducer, Fun) ->
     receive
 	stop -> 
 	    io:format("Mapper ~p stopping~n", [self()]),
+      mapper_loop(Reducer),
 	    ok;
-	....
+  {data, Data} ->
+      %%Actually perform data manipulation
+      %%and send to reducer
+      io:format("Mapper received data, processing"),
+      data_async(Reducer, Fun(Data)),
+      mapper_loop(Reducer, Fun);
 	Unknown ->
-	    io:format("unknown message: ~p~n",[Unknown]), 
+	    io:format("unknown message in mapper: ~p~n",[Unknown]), 
 	    mapper_loop(Reducer, Fun)
     end.
